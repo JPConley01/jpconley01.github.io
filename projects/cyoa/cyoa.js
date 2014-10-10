469c337bb283a5b7104790466be980c97511db87
John Conley <jpconley01@gmail.com>
2014-10-10 00:59:05 -0400

    update to use jquery

--- projects/cyoa/cyoa.js
+++ projects/cyoa/cyoa.js
@@ -1,24 +1,43 @@
-confirm("I am ready to play!");
-var age = prompt ("what's your age?");
-if (age < 13){
-    alert("You're allowed to play, but I take no responsibility for any mental damage.");
-}else
-{
-    alert ("Play on!");
-}
-alert("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
-alert("Suddenly, Bieber stops and says, 'Who wants to race me?'");
-var userAnswer = prompt ("Do you want to race Bieber on stage?");
-if (userAnswer = ("Yes")){
-        alert("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
-}
-else{
-    alert("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'") ;   
-}    
-var feedback = prompt("Please rate my game!Scale of 1 to 10 please!");
-if (feedback > 8){
-    alert("Thank you! We should race at the next concert!");
-}
-else{
-    alert("I'll keep practicing coding and racing.");
-}
+$("#hide1").hide();
+$("#hide2").hide();
+$("#hide3").hide();
+$("#hide4").hide();
+
+var play = function() {
+    $("#hide1").show();
+};
+
+var getAge = function() {
+    var age;
+    age = $("#agePrompt").val();
+    $("#line1").append("<p>You are age "+age+", good for you!</p>");
+    if(age > 13) {
+        $("#line1").append("<p>You're allowed to play, but I take no responsibility for any mental damage.</p>");
+    } else {
+        $("#line1").append("<p>Play on!</p>");
+    }
+    $("#line1").append("<p>You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'</p>");
+    $("#line1").append("<p>Who wants to race me?</p>");
+    $("#hide2").show();
+};
+
+var doRace = function(yesno) {
+    if(yesno) {
+        $("#line2").append("<p>You and Bieber start racing. It's neck and neck! You win by a shoelace!</p>");
+    } else {
+        $("#line2").append("<p>Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'</p>");
+    }
+    $("#hide3").show();
+};
+
+var rate = function() {
+    var rating;
+    rating = $("input[name='rating']:checked").val();
+    $("#feedback").append("<p>You rated it at: "+rating+"/10</p>");
+    if(rating > 8) {
+        $("#feedback").append("<p>Thank you! We should hang out lolol</p>");
+    } else {
+        $("#feedback").append("<p>Fuck you. Die in a fire.</p>");
+    }
+    $("#hide4").show();
+};
