84285d8e01def4efee4d462af82d17df381ec857
John Conley <jpconley01@gmail.com>
2014-10-09 23:16:20 -0400

    change console.log to alert

--- projects/cyoa/cyoa.js
+++ projects/cyoa/cyoa.js
@@ -1,24 +1,24 @@
 confirm("I am ready to play!");
 var age = prompt ("what's your age?");
 if (age < 13){
-    console.log ("You're allowed to play, but I take no responsibility for any mental damage.");
+    alert("You're allowed to play, but I take no responsibility for any mental damage.");
 }else
 {
-    console.log ("Play on!");
+    alert ("Play on!");
 }
-console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
-console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+alert("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+alert("Suddenly, Bieber stops and says, 'Who wants to race me?'");
 var userAnswer = prompt ("Do you want to race Bieber on stage?");
 if (userAnswer = ("Yes")){
-        console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+        alert("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
 }
 else{
-    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'") ;   
+    alert("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'") ;   
 }    
 var feedback = prompt("Please rate my game!Scale of 1 to 10 please!");
 if (feedback > 8){
-    console.log("Thank you! We should race at the next concert!");
+    alert("Thank you! We should race at the next concert!");
 }
 else{
-    console.log("I'll keep practicing coding and racing.");
+    alert("I'll keep practicing coding and racing.");
 }
